20b1bb5b1e4b42016cd01290031af6384d71faf8
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2024-08-24 11:49:51 +0200

    Random walking

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,56 @@
 //! This example demonstrates bounding volume intersections.
 
-use bevy::{color::palettes::css::*, prelude::*};
-use rand::prelude::*;
+use std::path;
+
+use bevy::{color::palettes::css::WHITE, prelude::*};
+use rand::{thread_rng, prelude::*};
 
 fn main() {
     App::new()
+        .insert_resource(Time::<Fixed>::from_seconds(0.01))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (spin, render_shapes))
-        .add_systems(FixedUpdate, draw_lines)
+        .add_systems(FixedUpdate, (draw_lines))
         .run();
 }
 
 #[derive(Component)]
-struct Spin;
+struct RandomWalk {
+    path: Vec<Vec2>,
+}
 
-fn spin(time: Res<Time>, mut query: Query<&mut Transform, With<Spin>>) {
-    for mut transform in query.iter_mut() {
-        transform.rotation *= Quat::from_rotation_z(time.delta_seconds() / 5.);
+trait Walker {
+    fn walk(&mut self);
+}
+
+const SCALE: f32 = 10.0;
+
+impl Walker for RandomWalk {
+    fn walk(&mut self) {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(-1.0..=1.0);
+        let y = rng.gen_range(-1.0..=1.0);
+        self.path.push(Vec2::new(x, y));
+
     }
 }
-
-#[derive(Component)]
-enum Shape {
-    Line(Segment2d),
-}
-
-#[derive(Component)]
-struct RandomWalk {
-    path: Vec<Dir2>,
-}
-
-#[derive(Component)]
-struct Walker;
 
 // draw lines based on the RandomWalk component
-fn draw_lines(mut gizmos: Gizmos, query: Query<(&RandomWalk)>) {
-    let scale = 10.;
-    let meme = query.iter().next().unwrap();
-    let path = &meme.path;
+fn draw_lines(mut gizmos: Gizmos, mut query: Query<&mut RandomWalk>) {
+    let mut walker  = query.iter_mut().next().unwrap();
+    walker.walk();
+    let path = &walker.path;
     let mut start = Vec2::new(0., 0.);
     for i in path.iter() {
-        let end = Vec2::new(start.x + i.x * scale, start.y + i.y * scale);
+        let end = Vec2::new(start.x + i.x * SCALE, start.y + i.y * SCALE);
         gizmos.line_2d(start, end,  WHITE);
         start = end;
-    }
-}
-
-fn render_shapes(mut gizmos: Gizmos, query: Query<(&Shape, &Transform)>) {
-    let color = GRAY;
-    for (shape, transform) in query.iter() {
-        let translation = transform.translation.xy();
-        let rotation = transform.rotation.to_euler(EulerRot::YXZ).2;
-        match shape {
-            Shape::Line(l) => {
-                gizmos.primitive_2d(l, translation, rotation, color);
-            }
-        }
     }
 }
 
 #[derive(Component)]
 enum DesiredVolume {
     Aabb,
-    Circle,
 }
 
 #[derive(Component, Deref, DerefMut, Default)]
@@ -72,9 +59,7 @@ struct Intersects(bool);
 const OFFSET_X: f32 = 125.;
 const OFFSET_Y: f32 = 75.;
 
-
 fn setup(mut commands: Commands) {
-    let mut rng = thread_rng();
 
     commands.spawn(Camera2dBundle::default());
     commands.spawn((
@@ -86,25 +71,20 @@ fn setup(mut commands: Commands) {
         Intersects::default(),
     ));
 
-    let mut randomWalk = RandomWalk {
+    let mut random_walk = RandomWalk {
         path: Vec::new()
     };
-    let vs = vec![
-        Dir2::from_xy(0., 1.),
-        Dir2::from_xy(0., -1.),
-        Dir2::from_xy(1., 0.),
-        Dir2::from_xy(-1., 0.),
-    ];
-    for _ in 0..10 {
-        let random = vs.choose(&mut rand::thread_rng()).unwrap();
-        randomWalk.path.push(*random.as_ref().unwrap())
+
+    let mut rng = thread_rng();
+
+    for _ in 0..2 {
+        let x = rng.gen_range(-1.0..=1.0);
+        let y = rng.gen_range(-1.0..=1.0);
+        random_walk.path.push(Vec2::new(x, y));
     }
-    commands.spawn((
-        Walker,
-        randomWalk
-    ));
+    commands.spawn(random_walk);
     commands.spawn(
-        TextBundle::from_section("", TextStyle::default()).with_style(Style {
+        TextBundle::from_section("Random walk", TextStyle::default()).with_style(Style {
             position_type: PositionType::Absolute,
             bottom: Val::Px(12.0),
             left: Val::Px(12.0),
