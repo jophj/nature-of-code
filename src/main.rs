3010e54c252bb4ee95e200c1bd26eeda8dd2a767
Giovanni Pinto <jop.mezzana@gmail.com>
2024-08-30 10:14:37 +0200

    WIP centered random walk

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod demos;
 
-use demos::random_walk::RandomWalkPlugin;
+// use demos::random_walk::RandomWalkPlugin;
+use demos::random_walk_centered::RandomWalkCenteredPlugin;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(RandomWalkPlugin)
+        .add_plugins(RandomWalkCenteredPlugin)
         .run();
 }
