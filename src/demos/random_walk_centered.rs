8cde827485a6b94cc860ecead17c6b5b6144037c
Giovanni Pinto <jop.mezzana@gmail.com>
2024-09-05 17:04:31 +0200

    Bounded random walker is go

--- src/demos/random_walk_centered.rs
+++ src/demos/random_walk_centered.rs
@@ -1,11 +1,10 @@
-use std::path;
-
 use bevy::{
     color::palettes::css::WHITE, prelude::*
 };
 use rand::{thread_rng, prelude::*};
 
 const SCALE: f32 = 10.0;
+const BOUNDARIES: Vec2 = Vec2::new(22., 22.);
 
 #[derive(Component)]
 struct Walk {
@@ -22,15 +21,42 @@ impl Walk {
 }
 
 trait Walker {
-    fn walk(path: &Vec<Vec2>, window: &Window) -> Vec2;
+    fn walk(path: &Vec<Vec2>, boundaries: &Vec2) -> Vec2;
 }
 
-struct RandomWalker;
-impl Walker for RandomWalker {
-    fn walk(_path: &Vec<Vec2>, _window: &Window) -> Vec2 {
+struct BoundedRandomWalker;
+impl Walker for BoundedRandomWalker {
+    fn walk(path: &Vec<Vec2>, boundaries: &Vec2) -> Vec2 {
         let mut rng = thread_rng();
         let x = rng.gen_range(-1.0..=1.0);
         let y = rng.gen_range(-1.0..=1.0);
+
+        let resultant = path.iter().fold(Vec2::new(0., 0.), |acc, x| Vec2::new(acc.x + x.x, acc.y + x.y));
+        println!("{:?} ", resultant);
+
+        let x: f32  = if resultant.x < -boundaries.x || resultant.x > boundaries.x {
+            // move towards the center
+            if resultant.x > 0. {
+                -1.0
+            } else {
+                1.0
+            }
+        } else {
+            // move in the same direction
+            x
+        };
+
+        let y = if resultant.y < -boundaries.y || resultant.y > boundaries.y {
+            // move towards the center
+            if resultant.y > 0. {
+                -1.0
+            } else {
+                1.0
+            }
+        } else {
+            // move in the same direction
+            y
+        };
 
         Vec2::new(x as f32, y as f32)
     }
@@ -38,12 +64,11 @@ impl Walker for RandomWalker {
 
 
 // draw lines based on the RandomWalk component
-fn walk(mut gizmos: Gizmos, mut query: Query<&mut Walk>, windows: Query<&Window>) {
+fn walk(mut gizmos: Gizmos, mut query: Query<&mut Walk>) {
     // log the window size
-    let window = windows.iter().next().unwrap();
     // info!("Window size: {:?}", window);
     for mut walker in query.iter_mut() {
-        let step = RandomWalker::walk(&walker.path, window);
+        let step = BoundedRandomWalker::walk(&walker.path, &BOUNDARIES);
         walker.append(step);
         let path = &walker.path;
         let mut start = Vec2::new(0., 0.);
@@ -68,7 +93,7 @@ fn setup(mut commands: Commands) {
     commands.spawn(Walk::new());
     commands.spawn(Walk::new());
     commands.spawn(
-        TextBundle::from_section("Random walk", TextStyle::default()).with_style(Style {
+        TextBundle::from_section("Bounded random walker", TextStyle::default()).with_style(Style {
             position_type: PositionType::Absolute,
             bottom: Val::Px(12.0),
             left: Val::Px(12.0),
