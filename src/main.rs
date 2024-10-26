e72e939515be22911a770b698a2a1768f0812ba1
Giovanni Pinto <giovanni.pinto@appliedblockchain.com>
2024-10-26 11:23:16 +0200

    Perlin Walker boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 mod demos;
 
 // use demos::random_walk::RandomWalkPlugin;
-use demos::levy_flight::LevyFlightPlugin;
 // use demos::random_walk_bounded::BoundedRandomWalkPlugin;
 // use demos::random_walk::RandomWalkPlugin;
+// use demos::levy_flight::LevyFlightPlugin;
+use demos::perlin_walker::PerlinWalkerPlugin;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(LevyFlightPlugin)
+        // .add_plugins(LevyFlightPlugin)
+        .add_plugins(PerlinWalkerPlugin)
         .run();
 }
